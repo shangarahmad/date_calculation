1747fbb839b01987acfe0c276857938b58b2b719
shangarahmad <72816991+shangarahmad@users.noreply.github.com>
2021-01-15 21:09:01 +0300

    Add files via upload

---
+++ Console_date_calculation/Console_date_calculation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console_date_calculation
+{
+    using System;
+
+    public class Program
+    {
+        public static int getAge(int month, int day, int year)
+        {
+            DateTime today = DateTime.Today;
+            int atthisDay = today.Day;
+            int atthisYear = today.Year;
+            int atthisMonth = today.Month;
+            int age = 0;
+            if (atthisMonth < month)
+            {
+                age -= 1;
+            }
+            else if (atthisMonth == month)
+            {
+                if (atthisDay < day)
+                {
+                    age -= 1;
+                }
+            }
+            atthisYear -= year;
+            age += atthisYear;
+            return age;
+        }
+        public static void Main()
+        {
+            int ageInYears = getAge(3, 3, 2000);
+            Console.WriteLine(ageInYears);
+        }
+    }
+}
